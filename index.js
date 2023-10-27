9bc052d49e4a22560d0549440d2da02516946747
TrishonBaidaya7399 <githubtrishon@gmail.com>
2023-10-28 02:29:54 +0600

    services added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,6 +7,8 @@ require('dotenv').config()
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c2jrci5.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,10 +21,73 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // Get the database and collection on which to run the operation
+    const serviceCollection = client.db("CarDoctor").collection("services");
+    const bookingCollection = client.db("CarDoctor").collection("bookings");
+    
+    // Get all services data
+    app.get('/services', async(req, res)=>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);    
+    })
+
+    app.get('/services/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query= { _id: new ObjectId(id) }
+        const options = {
+          projection: {title: 1, price: 1, service_id: 1, img: 1}
+        }
+        const result = await serviceCollection.findOne(query, options);
+        res.send(result);
+    })
+
+    //set customer service data
+    app.post('/bookings', async(req, res)=>{
+      const booking = req.body;
+     const result = await bookingCollection.insertOne(booking);
+     res.send(result);
+    } )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
